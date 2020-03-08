297bc4209d36bd28448a713d65f34fa7305db13f
Luis Torres <luigi1209@hotmail.com>
2020-03-08 00:21:16 -0500

    Added ASP.NET Core MVC project

---
+++ src/Regionalizer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Regionalizer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
